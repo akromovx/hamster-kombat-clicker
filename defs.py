d0f44c8fc94164b1ddb404df54ae353de6809de9
akromovx <r3s1zetv@gmail.com>
2024-07-19 04:15:40 +0300

    fix getTimestamp()

--- defs.py
+++ defs.py
@@ -7,9 +7,9 @@ def getTimestamp():
     return calendar.timegm(time.gmtime())
 
 
-AUTH_TOKEN = 'YOUR_TOKEN_HERE'
+AUTH_TOKEN = 'API_TOKEN_HERE'
 CLICKER_COUNT = 1500
 
 API_URL = "https://api.hamsterkombatgame.io/clicker/tap"
-PAYLOAD = json.dumps({"count": CLICKER_COUNT, "availableTaps": 0, "timestamp": getTimestamp})
+PAYLOAD = json.dumps({"count": CLICKER_COUNT, "availableTaps": 0, "timestamp": getTimestamp()})
 HEADERS = {'Authorization': f'Bearer {AUTH_TOKEN}', 'Content-Type': 'application/json'}
